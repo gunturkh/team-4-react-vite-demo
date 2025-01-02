fef1db0278d3c0385f58e2e33931e8c958055f46
Guntur KH <guntur.kh@gmail.com>
2025-01-02 21:29:22 +0700

    feat: add text handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
-import { useState } from 'react'
-import Button from './Button'
-import Header from './Header'
-import './App.css'
-import Footer from './Footer'
-import Description from './Description'
+import { useState } from "react";
+import Button from "./Button";
+import Header from "./Header";
+import "./App.css";
+import Footer from "./Footer";
+import Description from "./Description";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
 
   const handleClick = (value: number) => {
-    setCount((count) => count + value)
-  }
+    setCount((count) => count + value);
+  };
+
+  const handleText = (value: string) => {
+    setText(value);
+  };
 
   return (
     <>
       <Header />
       <div className="card">
+        <div style={{ color: "green", fontSize: "20px", fontWeight: 700 }}>
+          Hello: {text}
+        </div>
+        <Button onClickProps={() => handleText('John')}>
+          Change text to Hello John
+        </Button>
+        <Button onClickProps={() => handleText("Jane")}>
+          Change text to Hello Jane
+        </Button>
         <Button onClickProps={() => handleClick(1)}>
-          <div style={{ color: 'red' }}>Count is {count}</div>
+          <div style={{ color: "red" }}>Count is {count}</div>
         </Button>
         <Button onClickProps={() => handleClick(-1)}>
           Decrement by 1: Count is {count}
@@ -28,12 +42,11 @@ function App() {
         <Button onClickProps={() => handleClick(-10)}>
           Decrement by 10: Count is {count}
         </Button>
-        
       </div>
       <Description />
-     <Footer/> 
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
